422b77fbdd7bb0737c435dea175bc5be5573584e
johnny_m <Jmeng1515@gmail.com>
2026-02-27 19:02:20 -0600

    Add files via upload

---
+++ cnn.py
@@ -0,0 +1,72 @@
+from torch import nn
+
+class CNN(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int, kernel_size: int, padding: int, image_size_x: int, image_size_y: int):
+        super().__init__()
+        self.block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape,
+                      out_channels=hidden_units,
+                      kernel_size=kernel_size,
+                      stride=1,
+                      padding=padding),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=kernel_size,
+                      stride=1,
+                      padding=padding),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2),
+        )
+        self.block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=kernel_size,
+                      stride=1,
+                      padding=padding),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=kernel_size,
+                      stride=1,
+                      padding=padding),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2)
+        )
+        self.features_conv = nn.Sequential(
+            self.block_1,
+            self.block_2,
+            self.block_2,
+            self.block_2[:-1]
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=int(hidden_units)*4,
+                      out_features=output_shape)
+        )
+
+        self.gradients = None
+
+        self.max_pool = nn.MaxPool2d(kernel_size=2,
+                                     stride=2)
+
+    def activations_hook(self, grad):
+        self.gradients = grad
+
+    def forward(self, x):
+        x = self.features_conv(x)
+
+        if x.requires_grad:
+            x.register_hook(self.activations_hook)
+
+        x = self.max_pool(x)
+        x = self.classifier(x)
+        return x
+    
+    def get_activations_gradient(self):
+        return self.gradients
+    
+    def get_activations(self, x):
+        return self.features_conv(x)
---
+++ main.py
@@ -0,0 +1,54 @@
+from dataset import prepare_dataset
+from cnn import CNN
+from train import train
+from eval import evaluate
+from torch import load
+from pathlib import Path
+
+def main():
+
+    train_dataloader, test_dataloader, val_dataloader, val_dataloader_for_eval = prepare_dataset()
+
+    # The model's parameters can be adjusted here. Only models trained using
+    # the same parameters as the ones you are currently using can be loaded properly
+
+    model = CNN(input_shape=3, 
+                hidden_units=8,
+                output_shape=1,
+                kernel_size=11,
+                padding=3,
+                image_size_x=160,
+                image_size_y=160)
+    
+    model_name = 'pneumonia_xray_model_v2' # Set the name of your model. If it already exists, 
+                                           # it will be loaded. If not, a new model will be trained
+                                           # and saved in the 'models' folder under this name.
+
+    num_epochs = 10
+
+    lr = 5e-4
+
+    if Path('models/' + model_name).is_file() == True:
+
+        model.load_state_dict(load('models/' + model_name, weights_only = False))
+
+        # train(train_dataloader, val_dataloader, model, model_name, num_epochs, lr) -- uncomment to
+        # continue training a pre-existing model.
+
+    else:
+
+        train(train_dataloader, val_dataloader, model, model_name, num_epochs, lr) # Trains a new model.
+
+
+    final_evaluation = False # When ready for the final evaluation, set this to true to evaluate the
+                             # model using the test set. Otherwise, the model will be evaluated using
+                             # the validation set.
+
+    if final_evaluation == True:
+        evaluate(model, test_dataloader, model_name)
+    else:
+        evaluate(model, val_dataloader_for_eval, model_name)
+
+if __name__ == "__main__":
+    main()
+
